a81c1685fb46ade413d5b76f133cd39f6e37bf00
rui.ribeiro <>
2020-10-27 23:11:48 +0000

    final

--- src/app/shared/components/error-message/error-message.component.ts
+++ src/app/shared/components/error-message/error-message.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-error-message',
   templateUrl: './error-message.component.html',
-  styleUrls: ['./error-message.component.css']
+  styleUrls: ['./error-message.component.css'],
 })
 export class ErrorMessageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
